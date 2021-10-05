60c839d469efe806ec2bee20cb1416ae90de0086
Harry Ji <jiharry@hotmail.com>
2021-10-04 18:55:43 -0700

    version 5.1 include imrpove trend

--- solution/db/seeds/clucks.js
+++ solution/db/seeds/clucks.js
@@ -1,13 +1,26 @@
+const { fake } = require("faker");
+const faker = require("faker")
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('clucks').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      const clucks=[];        
+      /*for (let i = 0; i < 30; i++) {        
+        clucks.push(
+          { 
+            //username: faker.name.firstName, 
+            content: faker.lorem.sentences,              
+          },
+        )
+      };*/
+      clucks.push(
+        { 
+          username: "firstName", 
+          content: "sentences"              
+        }
+      )
+      return knex('clucks').insert(clucks);      
     });
 };
